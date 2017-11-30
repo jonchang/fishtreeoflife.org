c7d4a82a1a75530897cf8906d5a780d4934470ad
Jonathan Chang <jchang641@gmail.com>
2017-11-29 19:04:14 -0800

    Fossils test

---
+++ scripts/generate_fossils.R
@@ -0,0 +1,37 @@
+#!/usr/bin/env Rscript
+
+library(ape)
+library(dplyr)
+library(readr)
+library(ggplot2)
+library(glue)
+
+tree <- read.tree("downloads/actinopt_12k_treePL.tre.xz")
+fossil_nodes <- read_csv("downloads/fossil/output_data.csv")
+
+fossil_nodes$idx <- seq_len(nrow(fossil_nodes))
+
+# Add a new column `node` with the node number of that calibration
+fossil_nodes <- group_by(fossil_nodes, group) %>% mutate(node = getMRCA(tree, c(left, right)))
+
+png("_assets/img/vertical_tree.png", width = 740, height = 740 * 3, antialias = "none", bg = "transparent")
+plot(tree, show.tip.label = FALSE, no.margin = TRUE)
+lastPP <- get("last_plot.phylo", envir = .PlotPhyloEnv)
+res <- fossil_nodes %>% mutate(x = lastPP$xx[node], y = lastPP$yy[node],
+                               devx = grconvertX(x, to = "device"),
+                               devy = grconvertY(y, to = "device")) %>% ungroup()
+dev.off()
+
+
+write_csv(res, "_data/fossil_data.csv")
+
+mdpath <- "_fossils/"
+dir.create(mdpath, recursive = T)
+
+for (ii in 1:nrow(res)) {
+    sink(file.path(mdpath, paste0(res$idx[ii], ".md")))
+    cat(paste0("---\ntitle: ", res$fossil[ii], "\n---\n"))
+    sink(NULL)
+}
+
+
