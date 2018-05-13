91e8d1d7176aab32b040b1dc76bbe447b36420a7
Jonathan Chang <jchang641@gmail.com>
2018-05-13 00:50:16 -0400

    Do orders too

--- scripts/generate_taxonomy.R
+++ scripts/generate_taxonomy.R
@@ -31,55 +31,51 @@ charsets <- readLines("downloads/final_alignment.partitions") %>% str_replace_al
 fossils <- read_csv("_data/fossil_data.csv")
 
 datapath <- "_data/"
-mdpath <- "_family/"
-downloadpath <- "downloads/family"
 
-dir.create(mdpath, recursive = T)
 dir.create(datapath, recursive = T)
-dir.create(downloadpath, recursive = T)
 
 tips <- str_replace_all(tre$tip.label, "_", " ")
 dna[[1]] <- str_replace_all(dna[[1]], "_", " ")
 
-generate_family_data <- function(family) {
+generate_rank_data <- function(df, current_rank, downloadpath, mdpath) {
     out <- list()
-    out$species <- family$genus.species
+    out$species <- df$genus.species
     out$sampled_species <- out$species[out$species %in% tips]
-    out$order <- unique(family$order)
-    family <- unique(family$family)
-    out$family <- family
+    out$order <- unique(df$order)
+    out$family <- unique(df$family)
+    rankname <- out[[current_rank]]
 
     out$num_rogues <- 0
     if (length(out$sampled_species) > 0) {
         wanted_spp <- dna[[1]][dna[[1]] %in% out$sampled_species] %>% str_replace_all(" ", "_")
         wanted_dna <- dna[[2]][dna[[1]] %in% out$sampled_species]
-        out$matrix_phylip <- file.path(downloadpath, paste0(family, ".phylip.xz"))
-        out$matrix_nexus <- file.path(downloadpath, paste0(family, ".nex.xz"))
+        out$matrix_phylip <- file.path(downloadpath, paste0(rankname, ".phylip.xz"))
+        out$matrix_nexus <- file.path(downloadpath, paste0(rankname, ".nex.xz"))
         make_phylip(out$matrix_phylip, wanted_spp, wanted_dna)
         make_nexus(out$matrix_nexus, wanted_spp, wanted_dna)
         if (length(out$sampled_species) > 2) {
             chrono <- get_rank_trees(tre, out$sampled_species)
             phylog <- get_rank_trees(tre, out$sampled_species)
-            out$chronogram <- file.path(downloadpath, paste0(family, ".tre"))
-            out$phylogram <- file.path(downloadpath, paste0(family, "_phylogram.tre"))
+            out$chronogram <- file.path(downloadpath, paste0(rankname, ".tre"))
+            out$phylogram <- file.path(downloadpath, paste0(rankname, "_phylogram.tre"))
             write.tree(chrono$pruned_tree, out$chronogram)
             write.tree(phylog$pruned_tree, out$phylogram)
             make_nexus(out$matrix_nexus, wanted_spp, wanted_dna, tree = chrono$pruned_tree, charsets = charsets)
             out$num_rogues <- chrono$num_rogues
             if (out$num_rogues > 0) {
-                out$chronogram_mrca <- file.path(downloadpath, paste0(family, "_mrca.tre"))
-                out$phylogram_mrca <- file.path(downloadpath, paste0(family, "_mrca_phylogram.tre"))
+                out$chronogram_mrca <- file.path(downloadpath, paste0(rankname, "_mrca.tre"))
+                out$phylogram_mrca <- file.path(downloadpath, paste0(rankname, "_mrca_phylogram.tre"))
                 write.tree(chrono$mrca_tree, out$chronogram_mrca)
                 write.tree(phylog$mrca_tree, out$phylogram_mrca)
             }
         }
     }
 
-    no_unbox <- c("species", "sampled_species")
+    no_unbox <- c("species", "sampled_species", "family", "order")
     for (nn in names(out)) {
         if (!nn %in% no_unbox) out[[nn]] <- unbox(out[[nn]])
     }
-    cat("---\n\n---\n", file = file.path(mdpath, paste0(family, ".md")))
+    cat("---\n\n---\n", file = file.path(mdpath, paste0(rankname, ".md")))
     out
 }
 
@@ -88,12 +84,20 @@ invisible(dna)
 invisible(tre)
 invisible(tre2)
 
-splat <- split(tax, tax$family)
-res <- parallel::mclapply(splat, generate_family_data)
+ranks <- c("family", "order")
 
-cat(toJSON(res), file = file.path(datapath, "family.json"))
+for (rank in ranks) {
+    downloadpath <- file.path("downloads", rank)
+    mdpath <- paste0("_", rank)
+    dir.create(downloadpath, recursive = TRUE)
+    dir.create(mdpath, recursive = TRUE)
 
-if (length(res) != length(splat)) q(status = 1)
+    splat <- split(tax, tax[[rank]])
+    res <- parallel::mclapply(splat, generate_rank_data, current_rank = "family", downloadpath = "downloads/family", mdpath = "_family")
+    cat(toJSON(res), file = file.path(datapath, paste0(rank, ".json")))
+    # Error out if Travis gives us grief
+    if (length(res) != length(splat)) q(status = 1)
+}
 
 q()
 
