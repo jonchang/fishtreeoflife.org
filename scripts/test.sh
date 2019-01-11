50aec32782ea096f24af2f18e12633d3975ac860
Jonathan Chang <jchang641@gmail.com>
2019-01-10 18:29:55 -0800

    Use html-proofer

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 
+set -eux
+
 # only error if stuff was found
 ! grep ERROR_ERROR_ERROR -lr _site
+
+# ensure internal links are valid
+bundle exec htmlproofer --allow-hash-href --disable-external --empty-alt-ignore --assume-extension ./_site
