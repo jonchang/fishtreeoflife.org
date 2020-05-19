0b8259e537d44caf7e6d44ee645fbb612c6757fd
Jonathan Chang <jchang641@gmail.com>
2020-05-19 12:48:24 +1000

    Install needed packages
    Why are these needed now?

--- scripts/fishtree_docs.sh
+++ scripts/fishtree_docs.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 
-docker run -i -v "$PWD"/fishtree:/fishtree rocker/tidyverse:latest R --slave << COMMANDS
+docker run -i -v "$PWD"/fishtree:/fishtree rocker/tidyverse:latest /bin/bash << EOBASH
+
+apt update
+apt install -y --no-install-recommends libmagick++-dev libgsl-dev
+
+R --slave << EOR
 options(repos = "https://cloud.r-project.org")
-install.packages("fishtree", dependencies = TRUE, Ncpus = 4)
+install.packages(c("fishtree", "pkgdown"), dependencies = TRUE, Ncpus = 4)
 requireNamespace("pkgdown")
 td <- tempdir()
 xx <- download.packages("fishtree", type = "source", dest = td)
@@ -10,4 +15,6 @@ tf <- xx[, 2]
 untar(tf, exdir = td, verbose = TRUE)
 extpath <- file.path(td, "fishtree")
 pkgdown::build_site(extpath, override = list(destination = "/fishtree"))
-COMMANDS
+EOR
+
+EOBASH
