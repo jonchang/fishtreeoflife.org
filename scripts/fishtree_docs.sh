784776f9fc8a19aba77448da95d847cd69ce8b57
Jonathan Chang <me@jonathanchang.org>
2020-10-31 20:38:23 +1100

    Use RStudio package manager to build R docs

--- scripts/fishtree_docs.sh
+++ scripts/fishtree_docs.sh
@@ -1,14 +1,25 @@
 #!/bin/bash
 
-docker run -i -v "$PWD"/fishtree:/fishtree rocker/tidyverse:latest /bin/bash << EOBASH
+docker run --rm -i -v "$PWD"/fishtree:/fishtree rocker/tidyverse:latest /bin/bash << EOBASH
 
-apt update
-apt install -y --no-install-recommends libmagick++-dev libgsl-dev libharfbuzz-dev libfribidi-dev
+set -euo pipefail
 
-R --slave << EOR
+apt-get update
+apt-get install -y --no-install-recommends libmagick++-6.q16-8 libgsl23 libharfbuzz0b libfribidi0 libglpk40
+
+R --no-echo << EOR
+# Use RStudio package manager for most dependencies to avoid building the world from source
+install.packages(c("fishtree", "pkgdown"), dependencies = TRUE, Ncpus = 4)
+
+# But reinstall these needed packages from source
 options(repos = "https://cloud.r-project.org")
 install.packages(c("fishtree", "pkgdown"), dependencies = TRUE, Ncpus = 4)
+
+# Ensure that pkgdown and fishtree were actually installed
 requireNamespace("pkgdown")
+requireNamespace("fishtree")
+
+# Unpack the source and build with pkgdown
 td <- tempdir()
 xx <- download.packages("fishtree", type = "source", dest = td)
 tf <- xx[, 2]
